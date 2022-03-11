6a88e2a5ee6ea3854e5fe9ae35464b686c0826a6
K Siraki <kevsiraki@gmail.com>
2022-03-11 15:20:36 -0800

    initial CMD push

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+
+
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+		<div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        <div class="row">
+            <div class="col-12 text-center pt-5">
+                <h1 class="display-one mt-5">{{ config('app.name') }}</h1>
+                <p>This awesome blog has many articles, click the button below to see them</p>
+                <br>
+                <a href="/blog" class="btn btn-outline-primary">Show Blog</a>
+            </div>
+        </div>
+    </div>
+@endsection
